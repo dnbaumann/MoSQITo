8b80e800174a764ec9f313a49bf1d0c923c1729f
wantysal <salome.wanty@gmail.com>
2020-09-21 15:09:11 +0200

    [CC] resampled signal length correction

--- mosqito/functions/resampling.py
+++ mosqito/functions/resampling.py
@@ -3,15 +3,17 @@
 Created on 18/09/2020
 """
 
-from scipy.io import wavfile
 from scipy import signal
+import numpy as np
 
 
-def signal_resample (audio):
-    """ Change a signal sampling frequency to 48 kHZ"""
-
-
-    audio = signal.resample(audio, 48000)
+def signal_resample (audio, fs):
+    """ Changes signal sampling frequency to 48 kHZ"""
+    
+    
+    duration = int(len(audio)/fs)
+    audio = signal.resample(audio, 48000*duration)
+    audio = audio.astype(np.int16)
     fs = 48000
     
     return fs, audio
